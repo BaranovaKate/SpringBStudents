481d351f3c58cc4a04bf498156c2441eb945ef52
BaranovaKate <katte.bar19@gmail.com>
2024-02-24 14:29:10 +0300

    Initial commit

---
+++ src/main/java/by/baranova/springbootstudents/SpringBStudentsApplication.java
@@ -0,0 +1,13 @@
+package by.baranova.springbootstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBStudentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBStudentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/baranova/springbootstudents/SpringBStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.baranova.springbootstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBStudentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
